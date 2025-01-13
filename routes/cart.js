45065c38ab884dd70a111861b7d3da4dc1f307d8
navidul hoque <hnavidul@gmail.com>
2025-01-13 21:16:01 +0600

    backend

--- routes/cart.js
+++ routes/cart.js
@@ -1,14 +1,10 @@
 import express from 'express'
-import { addToCart, getCart, removeFromCart, updateCart } from '../controller/cart.js'
+import { getCart, updateCart } from '../controller/cart.js'
 
 const router = express.Router()
 
 router.post("/get", getCart)
 
 router.put("/update", updateCart)
-
-router.put("/addAndUpdate", addToCart)
-
-router.put("/removeAndUpdate", removeFromCart)
 
 export default router
